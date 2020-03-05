09e5f41378a7bcda519cdcf8ee9f36d7b0ceb98d
Jcordobes <jcordobescarmona@outlook.es>
2020-03-05 14:11:20 +0100

    Mi proyecto multimedia, primer intento

---
+++ src/Contenido.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { Route }  from "svelte-routing";
+  import Inicio     from "./Inicio.svelte";
+  import Peliculas  from "./Peliculas.svelte";
+  import Clientes   from "./Clientes.svelte";
+</script>
+
+<style>
+  main {
+    padding: 20px;
+    padding-top: 50px;
+  }
+</style>
+
+<main id="contenido">
+  <Route path="/" component={Inicio} />
+  <Route path="/peliculas" component={Peliculas} />
+  <Route path="/clientes" component={Clientes} />
+</main>
---
+++ src/Peliculas.svelte
@@ -0,0 +1,57 @@
+<script>
+  import { onMount, getContext } from "svelte";
+  import { jsonData }            from "./store.js";
+
+  import Buscar                  from "./Buscar.svelte";
+  import Pelicula                from "./Pelicula.svelte";
+  import Boton                   from "./Boton.svelte";
+
+  const URL = getContext("URL");
+
+  let busqueda = "";
+  let pelicula = {};
+
+  onMount(async () => {
+    const response = await fetch(URL.articulos);
+    const data = await response.json();
+    $jsonData = data;
+  });
+
+  $: regex = new RegExp(busqueda, "i");
+  $: datos = busqueda 
+    ? $jsonData.filter(item => regex.test(item.nombre))
+    : $jsonData;
+
+</script>
+
+<style>
+  .container {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: left;
+    flex-wrap: wrap;
+  }
+</style>
+
+<h1>ARTÍCULOS</h1>
+<Buscar bind:busqueda />
+
+<div class="container">
+  <Pelicula bind:pelicula>
+    <div style="text-align: right">
+      <Boton documento={pelicula} tipo="insertar" coleccion="peliculas" />
+    </div>
+  </Pelicula>
+</div>
+
+<div class="container">
+  {#each datos as pelicula}
+    <Pelicula {pelicula}>
+      <div style="text-align: right">
+        <Boton documento={pelicula} tipo="modificar" coleccion="peliculas" />
+        <Boton documento={pelicula} tipo="eliminar"  coleccion="peliculas" />
+      </div>
+    </Pelicula>
+  {/each}
+</div>
